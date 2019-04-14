5331ca2b10ef3d3d149f0901400d72224b124a1a
Ricardo <ricardoy@gmail.com>
2019-04-14 01:46:46 -0300

    Adding draggable components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+// @flow
 import React, { Component } from 'react';
-import SimpleMap from './map/SimpleMap'
+import SimpleMap from './util/SimpleMap'
+import MapForm from './util/MapForm'
+import Button from 'react-bootstrap/Button';
+import Draggable from 'react-draggable';
+
 import './App.css'
+import { relative } from 'path';
 
 class App extends Component {
-  render() {
-    return (      
-      <div className="full">
-        <SimpleMap/>      
-      </div>
+    render() {
+        return (
+            
+            <div className="full">
+                <Draggable>
+                    <div style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        zIndex: 200
+                    }}>
+                        <MapForm />
+                    </div>
+                </Draggable>
+                
+                <SimpleMap/>
+            </div>
+        
     );
   }
 }
--- src/util/SimpleMap.jsx
+++ src/util/SimpleMap.jsx
@@ -1,5 +1,7 @@
+// @flow
 import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import Button from 'react-bootstrap/Button';
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -8,10 +10,11 @@ class SimpleMap extends Component {
 
   render() {
     return (        
-        <Map center={this.props.position} zoom={13}>
+        <Map center={this.props.position} zoom={13} style={{zIndex: 100}}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                style={{zIndex: -1}}
             />
             <Marker position={this.props.position}>
                 <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
